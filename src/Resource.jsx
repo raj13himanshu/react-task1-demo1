19ce9ab1f24685ee49e52ef9830b5c89b8d3ed04
Himanshu Raj <63577940+raj13himanshu@users.noreply.github.com>
2021-12-21 11:43:21 +0530

    Add files via upload

---
+++ src/Resource.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Resource = () => {
+  return (
+    <>
+      <div>
+        <h1 className="design123"> Resource Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default Resource;
